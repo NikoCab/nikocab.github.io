0262cc346a04da134da1d834ed5cfd83dc39abaf
NikoCab <119622931+NikoCab@users.noreply.github.com>
2023-01-24 11:23:13 -0400

    reformat files

---
+++ helloJS/hello.js
@@ -0,0 +1,8 @@
+console.log('hola mundo')
+let costBread = 1;
+let costButter = 3;
+ 
+console.log(`Your total is: ${costBread+costButter}`);
+console.log(2 + function() {return 3} ())
+let x= 14 %2
+console.log(x)
